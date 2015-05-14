b07eb2e07b113cdf4339da45b73d35ebccdeedcf
Lorenzo De Nobili <elledienne@gmail.com>
2015-05-14 09:57:06 +0200

    oK

--- build_arrays.js
+++ build_arrays.js
@@ -3,22 +3,24 @@
  **/
 
 // Create an empty array.
-
+var a = [];
 
 // Create an array of the colors of the rainbow.
-
+var rainbowColors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 // Create an array of gems.
+var gems = ["diamond", "ruby"];
 
 
 // Create an array of military ranks.
-
+var ranks = ["caporal", "sergent", "leuteneant"];
 
 // Create an array of road signs.
+var signs = ["stop"];
 
 
 // Create an array containing components of a tree.
-
+var tree = ["roots", "leaf"];
 
 // Create an array of prime numbers less than 20.
 
